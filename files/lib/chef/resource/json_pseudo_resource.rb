5205581c82adb6ffa031b6137d4b7f194274884c
Chris Doherty <cdoherty@chef.io>
2015-11-06 19:53:43 -0800

    Move some functions to a resource superclass.

--- files/lib/chef/resource/json_pseudo_resource.rb
+++ files/lib/chef/resource/json_pseudo_resource.rb
@@ -1,33 +1,45 @@
 require 'json'
 
 require 'cheffish'
-require 'chef/resource/json_pseudo_resource'
 
 class Chef
 class Resource
-class TerraformExecute < Chef::Resource::JSONPseudoResource
+class JSONPseudoResource < Chef::Resource
 
-  resource_name :run_terraform
-  default_action :graph
+  resource_name :json_pseudo_resource
+
+  allowed_actions [:nothing]
 
   property :json_blob, String, required: true
-  property :refresh, [TrueClass, FalseClass], default: false
 
   property :tmpdir, String, default: "/tmp"
   property :canonical_name, String, default: lazy { name.gsub(/\s+/, '_') }
-  property :tf_filename, String, default: lazy { "#{canonical_name}.tf.json" }
-  property :json_path, String, default: lazy { ::File.join(tmpdir, tf_filename) }
-  property :data_bag_name, String, default: lazy { canonical_name }
-  property :tfstate_file, String, default: lazy { ::File.join(tmpdir, TFSTATE_FILENAME) }
+  property :json_filename, String, default: lazy { raise "override json_filename in subclass" }
+  property :full_json_path, String, default: lazy { ::File.join(tmpdir, tf_filename) }
 
-  TFSTATE_FILENAME = "terraform.tfstate"
+  property :data_bag_name, String, default: lazy { canonical_name }
 
   # add a CCR handler to delete the temporary tfstate directory.
 
   # magic invocation to be able to use resources inside helper functions.
   declare_action_class.class_eval do
-    def save_tfdata
-      ruby_block "save tf data in data bag #{canonical_name}" do
+    def write_json_file
+      file json_path do
+        content json_blob
+      end
+    end
+
+    def slurp(path)
+      raise ArgumentError, "Empty path in slurp()." if path.to_s.empty?
+      ::File.open(path) { |f| f.read }
+    end
+
+    def ensure_data_bag
+      chef_data_bag data_bag_name
+    end
+
+    def save_data(key, data, data_bag=canonical_name)
+      ruby_block "save tf data in data bag #{data_bag}" do
         block do
           data = {
             "id" => canonical_name,
@@ -48,7 +60,7 @@ class TerraformExecute < Chef::Resource::JSONPseudoResource
       end
     end
 
-    def load_tfdata
+    def load_data(key, data, data_bag=canonical_name)
       ruby_block "load tf data from data bag #{canonical_name}" do
         block do
           begin
@@ -64,45 +76,6 @@ class TerraformExecute < Chef::Resource::JSONPseudoResource
         end
       end
     end
-  end
-
-  action :graph do
-
-    write_json_file
-
-    execute "Terraform block '#{name}'" do
-      command "terraform graph"
-      cwd tmpdir
-    end
-  end
-
-  [:plan, :apply].each do |tf_command|
-    action tf_command do
-
-      os_tmpdir = Dir.mktmpdir
-      begin
-        tmpdir os_tmpdir
-
-        tf_cli_command = "terraform #{tf_command} --refresh=#{refresh}"
-
-        ensure_data_bag
-        write_json_file
-
-        load_tfdata
-
-        execute "Terraform block '#{name}'" do
-          command tf_cli_command
-          cwd tmpdir
-        end
-
-        save_tfdata
-      end
-      log "Terraform files can be found in #{tmpdir}"
-    end
-  end
-
-  action :noop do
-    log "noop noop noop noop"
   end
 end
 end
--- files/lib/terrachef.rb
+++ files/lib/terrachef.rb
@@ -2,6 +2,8 @@ require "terrachef/version"
 
 # TODO: figure out how to do this right.
 $: << File.dirname(__FILE__)
+
 require 'attribute_pairs'
 require 'terraform_compile'
+require 'chef/resource/json_pseudo_resource.rb'
 require 'chef/resource/terraform_execute'
